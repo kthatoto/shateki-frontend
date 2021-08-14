c3a71ea8dede48f36339eb4849fd9fba6130dddd
kenta.yamamomo <yamamoto7.pgming@gmail.com>
2021-08-15 05:20:13 +0900

    add: argument

--- src/canvas/shateki/createSound.ts
+++ src/canvas/shateki/createSound.ts
@@ -1,6 +1,7 @@
-export default (sound: string) => {
+export default (sound: string, volume: number = 1) => {
   const audio = new Audio(sound)
   audio.currentTime = 0
+  audio.volume = volume
   audio.loop = false
   audio.play()
 }
